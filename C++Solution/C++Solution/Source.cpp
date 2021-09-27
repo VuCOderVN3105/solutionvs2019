e0115ccf97d6d466d8457cd3e554f836fef06cf1
VuCOderVN3105 <luuvu3477@gmail.com>
2021-09-27 13:00:18 +0700

    Add Solution VS 2019

---
+++ C++Solution/C++Solution/Source.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "constan.h"
+using namespace constandvar;
+using namespace std;
+
+int main()
+{
+
+	return 0;
+}
---
+++ C++Solution/C++Solution/constan.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef _CONSTAN_
+#define _CONSTAN_
+
+namespace constandvar {
+	const int a = 1 + 2 / 3;
+	int b = a + a;
+	bool loop{ true };
+}
+
+#endif // !1
